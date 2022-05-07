3362d9d2c2e74665a69edb178f8ec68955851a71
durga0702 <durga@sparkouttech.com>
2022-05-07 11:42:08 +0530

    network change and add new network

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/**
+ * app component
+ */
 export class AppComponent implements OnInit {
-  public ethereum:any;
+  public ethereum:any = window['ethereum'];
   public walletAddress:any;
   public chainNetwork:any;
+  public netId:any;
+  public changedChainId:any;
+  public findingChainNetwork:any;
+ public id:any;
+ public net:any;
+  /**
+   * wallet connect
+   */
 
-  
+
   public connectWallet(){
     this.ethereum = window['ethereum']
     this.ethereum.request({ method: 'eth_requestAccounts' }).then((res: any)=>{
       console.log(res)
-      this.walletAddress = res[0]
+      this.walletAddress = res[0];
     }),
     (err: any)=>{
       console.log(err)
     }
-   
-  }
-  public chainId(){
+    this.networkChangeListener()
+   }
+  /**
+   * get chain id
+   */
+  public getChainId(){
     this.ethereum.request({ method: 'eth_chainId' }).then((res)=>{
       console.log(res);
       const networkId = res;
@@ -31,7 +45,12 @@ export class AppComponent implements OnInit {
     })
    
   }
+  /**
+   * 
+   * @param {string} id find network
+   */
   public netWork(id){
+    console.log('entered')
     switch(id) {
       case '0x1':
         this.chainNetwork = 'Ethereum Main Network (Mainnet)';
@@ -51,13 +70,75 @@ export class AppComponent implements OnInit {
           default:
             this.chainNetwork = 'unknown';
     }
-     
+   }
+  /**
+   * get net id
+   * @param {any} event 
+   */
+  public changeNetworkId(event){
+    console.log(event.target.value)
+     this.netId = event.target.value;
+     this.changeNetwork()
   }
-//  public changeNetwork(event){
-//    console.log(event.target.value)
-
-//   }
- ngOnInit(): void {
+  /**
+   * network change 
+   
+   */
+async changeNetwork(){
+  this.ethereum = window['ethereum']
+  try {
+    this.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: this.netId }],
+    });
+    
+  } catch (err) {
+    console.log(err);
+  }
+}
+  /**
+   * network change listener
+   */
+  networkChangeListener(){
+    this.ethereum.on('chainChanged', (chain) => {
+     setTimeout(() => {
+      this.changedChainId = chain;
+      console.log("check",this.changedChainId)
+      this.netWork(chain);
+     }, 1000);
+        
+     
+     });
+    }
+  /**
+   * store data
+   */
+   storeNetWorkData(id,net){
+     localStorage.setItem('id',id);
+     localStorage.setItem('network',net);
+    }
+    /**
+     * add new network
+     */
+     addNewNetWork(){
+       console.log('clicked')
+      this.ethereum.request({
+        method: 'wallet_addEthereumChain',
+         params: [
+          {
+            chainId: '0x64',
+            chainName: ' xDai MainNet',
+            rpcUrls: ['https://rpc.xdaichain.com/'] /* ... */,
+          },
+        ],
+       });
+    }
+ 
+  ngOnInit(): void {
       console.log(window['ethereum']);
+      
+      setInterval(() => {
+        this.networkChangeListener();
+      },1000)
      }
 }
